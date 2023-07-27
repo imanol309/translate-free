f18230a4932c093de6e58137fb1d3abe989d89ff
imanol309 <imanolguzman309@gmail.com>
2023-07-27 14:31:18 -0400

    Solucionando error de clark con la tiempo de mi pc

--- src/components/btnClark/btnClark.tsx
+++ src/components/btnClark/btnClark.tsx
@@ -1,10 +1,11 @@
 "use client";
+
 import { UserButton, useUser } from "@clerk/nextjs";
 import Link from "next/link";
 
 function BtnClark() {
-  const { isSignedIn } = useUser();
-
+  const { isSignedIn, user } = useUser();
+  console.log(user?.fullName);
   if (!isSignedIn) {
     return (
       <Link href="/sign-in" className="text-white mx-10 cursor-pointer">
@@ -12,7 +13,12 @@ function BtnClark() {
       </Link>
     );
   }
-  return <UserButton afterSignOutUrl="/" />;
+  return (
+    <div className="flex justify-center items-center gap-3 cursor-pointer">
+      <UserButton afterSignOutUrl="/" />
+      <h4>{user?.fullName}</h4>
+    </div>
+  );
 }
 
 export default BtnClark;
